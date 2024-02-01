f818ac2d22c780e023a7186e8741408a394e1262
Mr. Carron <151862977+jcarron-smcdsb@users.noreply.github.com>
2024-02-01 09:20:06 -0500

    updated reset toggle

--- main.ts
+++ main.ts
@@ -1,36 +1,50 @@
 input.onButtonPressed(Button.A, function () {
-    T += 0.1
+    T += -0.1
 })
 input.onButtonPressed(Button.AB, function () {
     reset = !(reset)
     basic.clearScreen()
+    T = 1
+    x = 0
+    y = 0
+    ys = [
+    0,
+    0,
+    0,
+    0,
+    0
+    ]
 })
 input.onButtonPressed(Button.B, function () {
-    T += -0.01
+    T += 0.1
 })
 let y = 0
+let ys: number[] = []
+let T = 0
+let x = 0
 let reset = false
 reset = false
-reset = false
 basic.clearScreen()
-let x = 0
-let T = 1
-let ys = [
+x = 0
+T = 1
+ys = [
 0,
 0,
 0,
 0,
 0
 ]
-while (!(reset)) {
-    y = Math.map(Math.sin(1 / (2 * T) * x), -1, 1, 0, 4)
-    ys.unshift(y)
-    ys.pop()
-    basic.clearScreen()
-    for (let index = 0; index <= 4; index++) {
-        led.plot(index, Math.round(ys[index]))
+basic.forever(function () {
+    while (!(reset)) {
+        y = Math.map(Math.sin(1 / (2 * T) * x), -1, 1, 0, 4)
+        ys.unshift(y)
+        ys.pop()
+        basic.clearScreen()
+        for (let index = 0; index <= 4; index++) {
+            led.plot(index, Math.round(ys[index]))
+        }
+        serial.writeValue("x", y)
+        basic.pause(100)
+        x += 1
     }
-    serial.writeValue("x", y)
-    basic.pause(100)
-    x += 1
-}
+})
